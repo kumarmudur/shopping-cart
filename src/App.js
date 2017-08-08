dc20d2199f59566fd2fd1c67a1613f722f95515b
kumarmudur <kumarmudur10@gmail.com>
2017-08-08 08:32:42 +0530

    test paragraph

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+    render() {
+        return(
+            <div>Hello Mahadeva</div>
+        )
+    }
 }
 
 export default App;
